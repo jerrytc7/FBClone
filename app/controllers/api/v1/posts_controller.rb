16ab60a09863e435b06b70dc040a6745cfe4e6e2
jerrytc7 <jerrykondner@gmail.com>
2022-01-13 10:41:07 -0500

    adding post crud ability

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,60 @@
+module Api 
+  module V1
+    class PostsController < ApplicationController
+      def index 
+        if user_signed_in?
+          render json: current_user.posts
+        else
+          render json: {}, status: 401
+        end 
+      end
+
+      def show
+        if user_signed_in?
+          post = Post.find_by(id: params[:id])
+          render json: post, status: :ok
+        else
+          render json: {}, status: 401
+        end
+      end
+
+      def create 
+        if user_signed_in? 
+          if post = current_user.posts.create(post_params)
+            render json: post, status: :created 
+          else 
+            render json: post.errors, status: 400
+          end
+        else 
+          render json: {}, status: 401
+        end
+      end
+
+      def update
+        if user_signed_in?
+          post = Post.find_by(id: params[:id])
+          post.update(post_params)
+          render json: post, status: :accepted
+        else
+          render json: {}, status: 401
+        end
+      end
+
+      def destroy
+        if user_signed_in?
+          post = Post.find_by(id: params[:id])
+          post.destroy
+          head :no_content, status: :ok
+        else
+          render json: {}, status: 401
+        end
+      end
+
+      private 
+
+      def post_params 
+        params.require(:post).permit(:title, :content)
+      end
+    end
+  end 
+end
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import {HashRouter, Routes, Route} from "react-router-dom";
+import PostList from './components/PostList';
+import NewPost from './components/NewPost';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<PostList />}/>
+          <Route exact path="/posts/new" element={<NewPost />}/>
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
