7bc21acf466cd3eed70223a970dfe3865d7fa675
jerrytc7 <jerrykondner@gmail.com>
2022-01-16 20:51:09 -0500

    guest html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user! 
+
+  def after_sign_in_path_for(resource)
+    app_path
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # get '/' => 'welcome#home'
   # get 'home' => 'welcome#home'
-  # get 'app' => 'welcome#app'
-  root "fallback#index"
+  get 'app' => 'welcome#app'
   namespace :api do 
     namespace :v1 do 
       resources :posts
     end 
   end
- get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+ get "*path", to: "welcome#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
